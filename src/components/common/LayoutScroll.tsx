8d42471088b09f5d5ba4d9d414f2e372752e0907
Npeka <npkhang22@clc.fitus.edu.vn>
2025-04-29 23:59:37 +0700

    style: add padding bottom layout scroll

--- src/components/common/LayoutScroll.tsx
+++ src/components/common/LayoutScroll.tsx
@@ -6,7 +6,7 @@ export default function LayoutScroll({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-full flex-col overflow-x-clip overflow-y-auto px-8">
+    <div className="flex h-full flex-col overflow-x-clip overflow-y-auto px-8 pb-4">
       <Space direction="vertical" size="large" style={{ width: '100%' }}>
         {children}
       </Space>
