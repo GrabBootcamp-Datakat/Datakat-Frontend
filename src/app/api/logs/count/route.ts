d6f797a1fd79876072f0fdf334730ec4822e15da
Npeka <npkhang22@clc.fitus.edu.vn>
2025-04-30 00:00:12 +0700

    perf: api log count

--- src/app/api/logs/count/route.ts
+++ src/app/api/logs/count/route.ts
@@ -1,55 +1,18 @@
 import { NextResponse } from 'next/server';
-import { parseLogsFromCsv } from '@/utils/parser';
 import { LogCountDto } from '@/types/logsType';
-import fs from 'fs/promises';
-import path from 'path';
-import dayjs from 'dayjs';
+import { logs } from '@/app/api/data';
 
-export async function GET(request: Request) {
+export async function GET() {
   try {
-    console.log('GET request received');
-    const { searchParams } = new URL(request.url);
-    const startDate = searchParams.get('startDate');
-    const endDate = searchParams.get('endDate');
-
-    console.log('startDate', startDate);
-
-    const filePath = path.join(
-      process.cwd(),
-      'public',
-      'data',
-      'Spark_2k.log_structured.csv',
-    );
-
-    // Read file asynchronously
-    const csvText = await fs.readFile(filePath, 'utf-8');
-    const logs = parseLogsFromCsv(csvText);
-
-    // Filter logs based on date range if provided
-    const filteredLogs =
-      startDate && endDate
-        ? logs.filter((log) => {
-            const logDate = dayjs(
-              `${log.Date} ${log.Time}`,
-              'YYYY-MM-DD HH:mm:ss',
-            );
-            return (
-              logDate.isValid() &&
-              logDate.isAfter(dayjs(startDate)) &&
-              logDate.isBefore(dayjs(endDate).add(1, 'day'))
-            );
-          })
-        : logs;
-
     const counts: LogCountDto = {
-      total: filteredLogs.length,
+      total: logs.length,
       INFO: 0,
       WARN: 0,
       ERROR: 0,
       DEBUG: 0,
     };
 
-    filteredLogs.forEach((log) => {
+    logs.forEach((log) => {
       const level = log.Level as keyof Omit<LogCountDto, 'defaultDateRange'>;
       counts[level] = (counts[level] || 0) + 1;
     });
