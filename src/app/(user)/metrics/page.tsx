76acc4eefe3e26725b15f9612b54f6ec114e6428
Npeka <npkhang22@clc.fitus.edu.vn>
2025-04-29 21:05:07 +0700

    feat: add empty metrics page

---
+++ src/app/(user)/metrics/page.tsx
@@ -0,0 +1,10 @@
+import LayoutScroll from '@/components/common/LayoutScroll';
+import PageTitle from '@/components/common/PageTitle';
+
+export default function MetricsPage() {
+  return (
+    <LayoutScroll>
+      <PageTitle title="Metrics" />
+    </LayoutScroll>
+  );
+}
