da25a24c5d765e7af6e8f2ddd6a29010303a10b7
Npeka <npkhang22@clc.fitus.edu.vn>
2025-04-29 21:04:52 +0700

    feat: add empty anomalies page

---
+++ src/app/(user)/anomalies/page.tsx
@@ -0,0 +1,10 @@
+import LayoutScroll from '@/components/common/LayoutScroll';
+import PageTitle from '@/components/common/PageTitle';
+
+export default function AnomaliesPage() {
+  return (
+    <LayoutScroll>
+      <PageTitle title="Anomalies" />
+    </LayoutScroll>
+  );
+}
