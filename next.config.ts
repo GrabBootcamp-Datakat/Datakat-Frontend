c047d89e7220196347cc363bef90bd75be04a779
Npeka <npkhang22@clc.fitus.edu.vn>
2025-05-09 22:19:33 +0700

    perf: optimize dev

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['localhost'],
-    unoptimized: process.env.NODE_ENV === 'development',
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV === 'production',
-  },
   experimental: {
     optimizeCss: true,
     scrollRestoration: true,
